04d78c0d80e57f9888cdca5dc735d872fb191331
hchevali <hdc.chevalier@gmail.com>
2019-04-18 13:51:08 +0200

    création facade

--- src/pack/Facade.java
+++ src/pack/Facade.java
@@ -1,5 +1,47 @@
 package pack;
+import java.util.Collection;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import enumerations.AntecedentsMedicaux;
+import enumerations.Cheveux;
+import enumerations.Loisirs;
+import enumerations.Peau;
+import enumerations.Yeux;
+
+@Singleton
+@LocalBean
 public class Facade {
 
+	@PersistenceContext
+	private EntityManager em;
+
+	public Facade(){
+		
+	}
+	
+	public void ajoutDonneur(String nom, String prenom,  int age, int taille, int poids, boolean sexe, boolean dispo,Yeux y,Cheveux c,Peau peau,Collection<Loisirs> l, Collection<AntecedentsMedicaux> am){
+		em.persist(new Donneur(nom, prenom, age,taille, poids, sexe, dispo, y, c, peau, l, am));
+	}
+
+	public void ajoutReceveur(String n, String p, int a, boolean s){
+		em.persist(new Receveur(n, p, a, s));
+	}
+	
+	public Collection<Donneur> listerDonneurs(){
+		TypedQuery<Donneur> req = em.createQuery("select p from Donneur p", Donneur.class);
+		return req.getResultList();
+		
+	}
+	
+	public Collection<Receveur> listerReceveurs(){
+		TypedQuery<Receveur> req = em.createQuery("select a from Receveur a", Receveur.class);
+		return req.getResultList();
+		
+	}
+	
 }
